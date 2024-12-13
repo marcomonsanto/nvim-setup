984bca681839192dd22bdafba1390dc5c70913bc
marco monsanto <marco.monsanto09@gmail.com>
2024-12-13 13:53:47 +0100

    update to theme

--- init.lua
+++ init.lua
@@ -1089,47 +1089,47 @@ require('lazy').setup({
     priority = 1000,
     config = function()
       require('nordic').setup {
-        -- bold_keywords = true,
-        -- swap_backgrounds = true,
+        bold_keywords = true,
+        swap_backgrounds = true,
         -- on_palette = function(palette)
-        --   -- palette.black0 = '#BF616A'
+        --   palette.black0 = '#BF616A'
         --
-        --   -- palette.orange = {
-        --   --   base = '#EBAE80',
-        --   --   bright = '#E8BFA6',
-        --   --   dim = '#E7B19C',
-        --   -- }
-        --   -- palette.green = {
-        --   --   base = '#B4D2A6',
-        --   --   bright = '#C4D7B5',
-        --   --   dim = '#A2C4A1',
-        --   -- }
-        --   -- palette.magenta = {
-        --   --   base = '#B4D2A6',
-        --   --   bright = '#E1B3D1',
-        --   --   dim = '#A2C4A1',
-        --   -- }
-        --   --
-        --   -- palette.orange = '#DB9E6D'
-        --   -- palette.cyan = '#A0D0D7'
-        --   -- palette.green = '#B4D2A6'
+        --   palette.orange = {
+        --     base = '#EBAE80',
+        --     bright = '#E8BFA6',
+        --     dim = '#E7B19C',
+        --   }
+        --   palette.green = {
+        --     base = '#B4D2A6',
+        --     bright = '#C4D7B5',
+        --     dim = '#A2C4A1',
+        --   }
+        --   palette.magenta = {
+        --     base = '#B4D2A6',
+        --     bright = '#E1B3D1',
+        --     dim = '#A2C4A1',
+        --   }
+        --
+        --   palette.orange = '#DB9E6D'
+        --   palette.cyan = '#A0D0D7'
+        --   palette.green = '#B4D2A6'
         --   -- palette.green.base = palette.cyan.base
         --   return palette
         -- end,
-        -- override = {
-        --   PmenuSel = { bg = '#CB775D' },
-        --   Visual = {
-        --     bg = '#D08770', -- Choose a color you like.,
-        --   },
-        --   CursorLineNr = {
-        --     fg = '#D08770',
-        --     bold = true,
-        --   },
-        -- },
-        -- telescope = {
-        --   -- Available styles: `classic`, `flat`.
-        --   style = 'flat',
-        -- },
+        override = {
+          PmenuSel = { bg = '#CB775D' },
+          Visual = {
+            bg = '#E7B19C', -- Choose a color you like.,
+          },
+          CursorLineNr = {
+            fg = '#D08770',
+            bold = true,
+          },
+        },
+        telescope = {
+          -- Available styles: `classic`, `flat`.
+          style = 'flat',
+        },
       }
       require('nordic').load()
     end,
