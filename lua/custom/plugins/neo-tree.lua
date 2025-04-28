a236a65ce92443869295a04b9fc292e7f2a189d8
Marco Monsanto <marco.monsanto09@gmail.com>
2025-04-28 10:58:32 +0200

    latest update

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,56 +1,56 @@
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  branch = 'v3.x',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  config = function()
-    require('neo-tree').setup {
-      window = {
-        mappings = {
-          ['<space>'] = 'none',
-          ['Y'] = {
-            function(state)
-              local node = state.tree:get_node()
-              local path = node:get_id()
-              vim.fn.setreg('+', path, 'c')
-            end,
-            desc = 'copy path to clipboard',
-          },
-        },
-        width = 40,
-        position = 'left',
-      },
-      default_component_configs = {
-        indent = {
-          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-          expander_collapsed = '',
-          expander_expanded = '',
-          expander_highlight = 'NeoTreeExpander',
-        },
-      },
-      filesystem = {
-        follow_current_file = {
-          enabled = true, -- This will find and focus the file in the active buffer every time
-          --               -- the current file is changed while the tree is open.
-          leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
-        },
-        filtered_items = {
-          visible = true,
-          show_hidden_count = true,
-          hide_dotfiles = false,
-          hide_gitignored = true,
-          hide_by_name = {
-            -- '.git',
-            -- '.DS_Store',
-            -- 'thumbs.db',
-          },
-          never_show = {},
-        },
-      },
-    }
-  end,
+  --   'nvim-neo-tree/neo-tree.nvim',
+  --   branch = 'v3.x',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+  --     'MunifTanjim/nui.nvim',
+  --     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  --   },
+  --   config = function()
+  --     require('neo-tree').setup {
+  --       window = {
+  --         mappings = {
+  --           ['<space>'] = 'none',
+  --           ['Y'] = {
+  --             function(state)
+  --               local node = state.tree:get_node()
+  --               local path = node:get_id()
+  --               vim.fn.setreg('+', path, 'c')
+  --             end,
+  --             desc = 'copy path to clipboard',
+  --           },
+  --         },
+  --         width = 40,
+  --         position = 'left',
+  --       },
+  --       default_component_configs = {
+  --         indent = {
+  --           with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+  --           expander_collapsed = '',
+  --           expander_expanded = '',
+  --           expander_highlight = 'NeoTreeExpander',
+  --         },
+  --       },
+  --       filesystem = {
+  --         follow_current_file = {
+  --           enabled = true, -- This will find and focus the file in the active buffer every time
+  --           --               -- the current file is changed while the tree is open.
+  --           leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+  --         },
+  --         filtered_items = {
+  --           visible = true,
+  --           show_hidden_count = true,
+  --           hide_dotfiles = false,
+  --           hide_gitignored = true,
+  --           hide_by_name = {
+  --             -- '.git',
+  --             -- '.DS_Store',
+  --             -- 'thumbs.db',
+  --           },
+  --           never_show = {},
+  --         },
+  --       },
+  --     }
+  --   end,
 }
