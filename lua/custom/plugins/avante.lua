11a9c9cd0ae6a2e6fefdb2d3b8912f32ceb00244
Marco Monsanto <marco.monsanto09@gmail.com>
2025-04-28 12:09:39 +0200

    update avante

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -1,68 +1,119 @@
 return {
-  'yetone/avante.nvim',
-  event = 'VeryLazy',
-  lazy = false,
-  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-  opts = {
-    -- add any opts here
-    auto_suggestions_provider = 'copilot',
-    provider = 'copilot',
+  {
+    'yetone/avante.nvim',
+    lazy = true,
+    event = 'VeryLazy',
+    build = 'make',
 
-    -- copilot = {
-    --   model = 'claude-3.5-sonnet',
-    -- },
-
-    -- model = 'o1-mini',
-
-    behaviour = {
-      auto_suggestions = true, -- Experimental stage
-    },
-
-    mappings = {
+    opts = {
+      provider = 'copilot',
+      auto_suggestions_provider = 'copilot',
+      copilot = {
+        model = 'claude-3.5-sonnet', -- Best balance of quality and speed
+        temperature = 0, -- 0 for deterministic responses
+        max_tokens = 8192, -- High token limit for longer responses
+        timeout = 60000, -- 60 seconds timeout for network operations
+        proxy = nil, -- Set to your proxy URL if needed
+        allow_insecure = false, -- Set to true only if you need to bypass SSL verification
+        disable_tools = {
+          'rag_search',
+          'python',
+          'create_file',
+          'rename_file',
+          'delete_file',
+          'create_dir',
+          'rename_dir',
+          'delete_dir',
+        },
+        code_editing_commands = true, -- Enable code editing commands
+        telemetry = false, -- Disable telemetry for privacy
+        split_threshold = 1024, -- Split large messages at this character count
+      },
+      hints = { enabled = true },
+      file_selector = {
+        provider = 'snacks',
+        provider_opts = {},
+      },
+      behaviour = {
+        -- enable_cursor_planning_mode = true,
+        auto_suggestions = true,
+        use_cwd_as_project_root = true,
+      },
+      mappings = {
+        suggestion = {
+          accept = '<Tab>',
+        },
+      },
       suggestion = {
-        accept = '<Tab>',
+        debounce = 500,
+        throttle = 500,
       },
     },
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = 'make',
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    'stevearc/dressing.nvim',
-    'nvim-lua/plenary.nvim',
-    'MunifTanjim/nui.nvim',
-    --- The below dependencies are optional,
-    'echasnovski/mini.pick', -- for file_selector provider mini.pick
-    'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
-    'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
-    'ibhagwan/fzf-lua', -- for file_selector provider fzf
-    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-    'zbirenbaum/copilot.lua', -- for providers='copilot'
-    {
-      -- support for image pasting
-      'HakonHarnes/img-clip.nvim',
-      event = 'VeryLazy',
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
+
+    dependencies = {
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        ft = function(_, ft)
+          vim.list_extend(ft, { 'Avante' })
+        end,
+      },
+      {
+        'folke/which-key.nvim',
+        opts = {
+          spec = {
+            { '<leader>a', group = 'ai' },
           },
-          -- required for Windows users
-          use_absolute_path = true,
         },
       },
     },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { 'markdown', 'Avante' },
+  },
+  {
+    'stevearc/dressing.nvim',
+    lazy = true,
+    opts = {
+      input = { enabled = false },
+      select = { enabled = false },
+    },
+  },
+  {
+    'saghen/blink.compat',
+    lazy = true,
+    opts = {},
+    config = function()
+      -- monkeypatch cmp.ConfirmBehavior for Avante
+      require('cmp').ConfirmBehavior = {
+        Insert = 'insert',
+        Replace = 'replace',
+      }
+    end,
+  },
+  {
+    'saghen/blink.cmp',
+    lazy = true,
+    opts = {
+      sources = {
+        default = { 'avante_commands', 'avante_mentions', 'avante_files' },
+        providers = {
+          avante_commands = {
+            name = 'avante_commands',
+            module = 'blink.compat.source',
+            score_offset = 90, -- show at a higher priority than lsp
+            opts = {},
+          },
+          avante_files = {
+            name = 'avante_files',
+            module = 'blink.compat.source',
+            score_offset = 100, -- show at a higher priority than lsp
+            opts = {},
+          },
+          avante_mentions = {
+            name = 'avante_mentions',
+            module = 'blink.compat.source',
+            score_offset = 1000, -- show at a higher priority than lsp
+            opts = {},
+          },
+        },
       },
-      ft = { 'markdown', 'Avante' },
     },
   },
 }
