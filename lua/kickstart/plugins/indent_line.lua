56461e28318a4361f124589d8fa4ab6b8929cdcb
Marco Monsanto <marco.monsanto09@gmail.com>
2025-10-26 20:02:55 +0100

    add indent

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,15 +1,15 @@
 return {
-  -- { -- Add indentation guides even on blank lines
-  --   'lukas-reineke/indent-blankline.nvim',
-  --   -- Enable `lukas-reineke/indent-blankline.nvim`
-  --   -- See `:help ibl`
-  --   main = 'ibl',
-  --   -- opts = {
-  --   -- scope = { enabled = true, show_exact_scope = true, highlight = { 'Function', 'Label' } },
-  --   -- indent = { char = '│' },
-  --   -- },
-  --   opts = {
-  --     scope = { char = '│', highlight = 'Function' },
-  --   },
-  -- },
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {
+      scope = { enabled = true, show_exact_scope = true, highlight = { 'Function', 'Label' } },
+      indent = { char = '│' },
+    },
+    -- opts = {
+    --   scope = { char = '│', highlight = 'Function' },
+    -- },
+  },
 }
